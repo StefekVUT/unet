312efb6d16a9dbbbc53d206662a838fe6210ed0c
smocko <stefan.mocko@fei.com>
2018-04-21 12:08:54 +0200

    augment relative path added

--- augment_data.py
+++ augment_data.py
@@ -1,11 +1,15 @@
 import Augmentor
+import os
 
-p=Augmentor.Pipeline("C:/Users/smocko/Desktop/generate_data/trainme")
-p.ground_truth("C:/Users/smocko/Desktop/generate_data/labelme")
-p.set_save_format("auto")
+dirpath = os.getcwd()
+print(dirpath)
+
+p=Augmentor.Pipeline(dirpath+'\\data\\trainme')
+p.ground_truth(dirpath+'\\data\\labelme')
+
 
 p.rotate(0.7, 25, 25)
 p.zoom(0.3, 1.1, 1.6)
 p.random_distortion(0.9, 8, 8, 2)
 
-p.sample(10000)
+p.sample(10)
